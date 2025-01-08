833f01a12d76bccac440ceede6dbfce96e7d1bf2
GuiTorres3000 <guilhermetorres3000@gmail.com>
2025-01-07 22:04:55 -0300

    setup: Interface de Login

---
+++ frontend/src/services/authService.ts
@@ -0,0 +1,19 @@
+/*import axios from 'axios';
+
+// Interface para o tipo de dados do usuário
+interface userProps {
+    email: string;
+    password: string;
+}
+
+// Função para login
+export const loginUser = async ({email, password}: userProps) => {
+    try {
+        const response = await axios.post('/api/login', data);
+        console.log('Login successful', response.data);
+    
+    } catch (error) {
+        throw new Error('Login failed');
+    }
+};
+*/
