06ffa587a195d998e1a7858bae2bec156eb58b45
GuiTorres3000 <guilhermetorres3000@gmail.com>
2025-01-08 10:36:50 -0300

    feat: Cadastro Funcional de Usuário

--- frontend/src/components/registerForm.tsx
+++ frontend/src/components/registerForm.tsx
@@ -1,4 +1,49 @@
+import { useState } from "react";
+import { registerUser } from "../services/authService";
+
+
 export default function registerForm() {
+  // Definindo estado que armazena os dados
+  // setFormData é uma função que atualiza o estado de formData
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  // Definindo estado que carrega mensagem de erro (pode ser string ou null, e começa como nulo) 
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  // Função para lidar com mudanças no input (acionada no onChange)
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // Copia a propriedade value do estado atual do input
+    const { name, value } = e.target;
+    // Atualiza o estado do formulário
+    setFormData((prevFormData) => ({
+      // Copia o estado anterior do formulário
+      ...prevFormData,
+      // Atualiza o campo correspondente com o novo valor
+      [name]: value,
+    }));
+  };
+  
+  // Envio das informações
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await registerUser(
+        formData.name, 
+        {
+        email: formData.email,
+        password: formData.password,
+        }
+    );
+      console.log("Usuário registrado com sucesso:", response);
+      setErrorMessage(null); // Limpa mensagem de erro ao sucesso
+    } catch (error) {
+      setErrorMessage("Erro ao registrar o usuário. Tente novamente.");
+    }
+  };
 
   const formHeader = (
     <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -20,7 +65,7 @@ export default function registerForm() {
           {formHeader}
 
           <div className="mt-12 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form action="#" method="POST" className="space-y-6">
+            <form onSubmit={handleSubmit} className="space-y-6">
             
             <div>
                 <label htmlFor="name" className="block text-sm/6 font-medium text-gray-900">
@@ -33,6 +78,8 @@ export default function registerForm() {
                     type="name"
                     required
                     autoComplete="name"
+                    value={formData.name}
+                    onChange={handleChange}
                     className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                   />
                 </div>
@@ -49,6 +96,8 @@ export default function registerForm() {
                     type="email"
                     required
                     autoComplete="email"
+                    value={formData.email}
+                    onChange={handleChange}
                     className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                   />
                 </div>
@@ -72,11 +121,17 @@ export default function registerForm() {
                     type="password"
                     required
                     autoComplete="current-password"
+                    value={formData.password}
+                    onChange={handleChange}
                     className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                   />
                 </div>
               </div>
-  
+
+              {errorMessage && (
+                <p className="text-red-500 text-sm">{errorMessage}</p>
+              )}
+
               <div>
                 <button
                   type="submit"
